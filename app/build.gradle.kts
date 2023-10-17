594c2fab37911a6b98e2cb76828ba7a942a2f5b3
oodorikku <audric_ang@dlsu.edu.ph>
2023-10-17 13:26:59 +0800

    Basic layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.mobdeve.s16.chua.andreikevin.dormdinner"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.mobdeve.s16.chua.andreikevin.dormdinner"
