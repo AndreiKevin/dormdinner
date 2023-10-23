7504d1f9bec2aa844b9c6e5b6936862ab70b74b8
oodorikku <audric_ang@dlsu.edu.ph>
2023-10-23 21:38:45 +0800

    Changed file names

---
+++ app/src/main/java/com/mobdeve/s16/chua/andreikevin/dormdinner/addRecipe.java
@@ -0,0 +1,15 @@
+package com.mobdeve.s16.chua.andreikevin.dormdinner;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class addRecipe extends AppCompatActivity{
+    String url;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_recipe);
+
+        url ="https://api.spoonacular.com/recipes/complexSearch";
+    }
+}
