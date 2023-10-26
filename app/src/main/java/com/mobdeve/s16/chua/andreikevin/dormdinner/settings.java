c55ef7e092d4b32eaa6e749630a92c83b975af43
Faith Griffin <faith_griffin@dlsu.edu.ph>
2023-10-26 12:51:24 +0800

    add(seekbar-settings): added seekbar settings logic

--- app/src/main/java/com/mobdeve/s16/chua/andreikevin/dormdinner/settings.java
+++ app/src/main/java/com/mobdeve/s16/chua/andreikevin/dormdinner/settings.java
@@ -3,16 +3,40 @@ package com.mobdeve.s16.chua.andreikevin.dormdinner;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class settings extends AppCompatActivity {
+
+    SeekBar sliderSettings;
+    TextView sliderValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
 
+        this.sliderSettings = (SeekBar) findViewById(R.id.sliderSettings);
+        this.sliderValue = (TextView) findViewById(R.id.sliderValue);
 
+        sliderSettings.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    // When the progress value has changed
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        sliderValue.setText(String.valueOf(progress));
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                    }
+                }
+                );
     }
 }
