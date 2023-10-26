205fe296b53bce0333afdb133c6a5577375cca04
Faith Griffin <faith_griffin@dlsu.edu.ph>
2023-10-26 17:29:08 +0800

    feature(settings): complete settings feature with add ingredients to avoid feature

--- app/src/main/java/com/mobdeve/s16/chua/andreikevin/dormdinner/settings.java
+++ app/src/main/java/com/mobdeve/s16/chua/andreikevin/dormdinner/settings.java
@@ -1,8 +1,13 @@
 package com.mobdeve.s16.chua.andreikevin.dormdinner;
 
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -12,6 +17,8 @@ public class settings extends AppCompatActivity {
 
     SeekBar sliderSettings;
     TextView sliderValue;
+    ImageButton btnAddAvoidIngredients;
+    EditText editAddAvoidIngredients;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,8 @@ public class settings extends AppCompatActivity {
 
         this.sliderSettings = (SeekBar) findViewById(R.id.sliderSettings);
         this.sliderValue = (TextView) findViewById(R.id.sliderValue);
+        this.btnAddAvoidIngredients = (ImageButton) findViewById(R.id.btnAddAvoidIngredients);
+        this.editAddAvoidIngredients = (EditText) findViewById(R.id.editAddAvoidIngredients);
 
         sliderSettings.setOnSeekBarChangeListener(
                 new SeekBar.OnSeekBarChangeListener() {
@@ -38,5 +47,30 @@ public class settings extends AppCompatActivity {
                     }
                 }
                 );
+
+        btnAddAvoidIngredients.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater layoutInflater = (LayoutInflater) getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                final View addView = layoutInflater.inflate(R.layout.new_ingredient, null);
+
+                final TextView text = (TextView) addView.findViewById(R.id.ingredient);
+                text.setText(editAddAvoidIngredients.getText().toString());
+
+                ImageButton delete_ingredient = (ImageButton) addView.findViewById(R.id.btnTrash);
+                final View.OnClickListener thisListener = new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ((LinearLayout) addView.getParent()).removeView(addView);
+                    }
+                };
+
+                delete_ingredient.setOnClickListener(thisListener);
+                /* TODO: add text entered checker logic */
+                ((LinearLayout) findViewById(R.id.avoidIngredientsParent)).addView(addView);
+                editAddAvoidIngredients.setText("");
+            }
+
+        });
     }
 }
